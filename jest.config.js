e3e3de48942d76f82a3b978d20ecb74ff200c7e4
Ayrton Porto <22572595+Ikusa0@users.noreply.github.com>
2024-01-25 16:06:21 -0300

    chore: add jest

---
+++ jest.config.js
@@ -0,0 +1,23 @@
+const config = {
+  roots: ['<rootDir>/src', '<rootDir>/tests'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.{ts,tsx}',
+    '!<rootDir>/src/main/**/*',
+    '!<rootDir>/src/**/index.ts',
+    '!**/*.d.ts'
+  ],
+  coverageDirectory: 'coverage',
+  testPathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+  ],
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.(ts|tsx)$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '@tests/(.*)': '<rootDir>/tests/$1',
+    '@/(.*)': '<rootDir>/src/$1',
+  }
+}
+
+export default config
