86729880bdb94f74d77be8f2ce6e3fd6dffb32ad
Ayrton Porto <22572595+Ikusa0@users.noreply.github.com>
2024-01-25 15:05:52 -0300

    feat: add Vite React-ts and Docker configurations

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import react from '@vitejs/plugin-react'
+import path from 'path'
+import { defineConfig } from 'vite'
+import svgr from 'vite-plugin-svgr'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), svgr()],
+  resolve: {
+    alias: { '@': path.resolve(__dirname, 'src'), '@tests': path.resolve(__dirname, 'tests') }
+  },
+  server: {
+    port: 5000,
+    host: true,
+    strictPort: true
+  }
+})
