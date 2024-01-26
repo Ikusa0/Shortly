c5f6845e0b882e11bd98dc54ead20a98e29ca531
Ayrton Porto <22572595+Ikusa0@users.noreply.github.com>
2024-01-26 15:40:58 -0300

    feat: add images index file

---
+++ src/presentation/images/index.ts
@@ -0,0 +1,5 @@
+export { default as BrandRecognitionIconSVG } from './icon-brand-recognition.svg?react'
+export { default as DetailedRecordsIconSVG } from './icon-detailed-records.svg?react'
+export { default as FullyCustomizableIconSVG } from './icon-fully-customizable.svg?react'
+export { default as WorkingIllustrationSVG } from './illustration-working.svg?react'
+export { default as LogoSVG } from './logo.svg?react'
