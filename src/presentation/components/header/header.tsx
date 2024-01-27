27c03d3f84a88633f00b877dfd1c87fb0a705077
Ayrton Porto <22572595+Ikusa0@users.noreply.github.com>
2024-01-27 13:08:55 -0300

    feat: add Header component

--- src/presentation/components/header/header.tsx
+++ src/presentation/components/header/header.tsx
@@ -1,13 +1,29 @@
+import Navigation from '@/presentation/components/navigation/navigation'
 import { LogoSVG } from '@/presentation/images'
-import React from 'react'
+import React, { useState } from 'react'
 import { AiOutlineMenu as MenuICON } from 'react-icons/ai'
 import Styles from './header-styles.module.scss'
 
+type StateTypes = {
+  navigation: boolean
+}
+
 const Header: React.FC = () => {
+  const [state, setState] = useState<StateTypes>({
+    navigation: false
+  })
+
+  const toggleNavigation = (): void => {
+    setState({ navigation: !state.navigation })
+  }
+
   return (
     <header className={Styles.header}>
       <LogoSVG className={Styles.logo} />
-      <MenuICON className={Styles.icon} />
+      <button onClick={toggleNavigation}>
+        <MenuICON className={Styles.icon} />
+      </button>
+      {state.navigation && <Navigation />}
     </header>
   )
 }
