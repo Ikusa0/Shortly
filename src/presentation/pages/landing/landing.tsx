7123519865758a3021b327f104016d414b2ee50c
Ayrton Porto <22572595+Ikusa0@users.noreply.github.com>
2024-01-27 14:14:58 -0300

    feat: add footer component

--- src/presentation/pages/landing/landing.tsx
+++ src/presentation/pages/landing/landing.tsx
@@ -1,12 +1,6 @@
+import Footer from '@/presentation/components/footer/footer'
 import Header from '@/presentation/components/header/header'
-import { LogoSVG } from '@/presentation/images'
 import React from 'react'
-import {
-  IoLogoFacebook as FacebookICON,
-  IoLogoInstagram as InstagramICON,
-  IoLogoPinterest as PinterestICON,
-  IoLogoTwitter as TwitterICON
-} from 'react-icons/io'
 import Presentation from './components/presentation/presentation'
 import SmallPresentation from './components/small-presentation/small-presentation'
 import Statistics from './components/statistics/statistics'
@@ -23,60 +17,7 @@ const Landing: React.FC = () => {
         <Statistics />
         <SmallPresentation />
       </article>
-      <footer>
-        <LogoSVG className={Styles.logo} />
-        <section className={Styles.footerItem}>
-          <h5>{`Features`}</h5>
-          <ul>
-            <li>
-              <a href="#">{`Link Shortening`}</a>
-            </li>
-            <li>
-              <a href="#">{`Branded Links`}</a>
-            </li>
-            <li>
-              <a href="#">{`Analytics`}</a>
-            </li>
-          </ul>
-        </section>
-        <section className={Styles.footerItem}>
-          <h5>{`Resources`}</h5>
-          <ul>
-            <li>
-              <a href="#">{`Blog`}</a>
-            </li>
-            <li>
-              <a href="#">{`Developers`}</a>
-            </li>
-            <li>
-              <a href="#">{`Support`}</a>
-            </li>
-          </ul>
-        </section>
-        <section className={Styles.footerItem}>
-          <h5>{`Company`}</h5>
-          <ul>
-            <li>
-              <a href="#">{`About`}</a>
-            </li>
-            <li>
-              <a href="#">{`Our Team`}</a>
-            </li>
-            <li>
-              <a href="#">{`Careers`}</a>
-            </li>
-            <li>
-              <a href="#">{`Contact`}</a>
-            </li>
-          </ul>
-        </section>
-        <section className={`${Styles.social} ${Styles.footerItem}`}>
-          <FacebookICON className={Styles.icon} />
-          <TwitterICON className={Styles.icon} />
-          <PinterestICON className={Styles.icon} />
-          <InstagramICON className={Styles.icon} />
-        </section>
-      </footer>
+      <Footer />
     </>
   )
 }
