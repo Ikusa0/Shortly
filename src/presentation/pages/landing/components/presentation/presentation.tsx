fcac0da5b644673ee1cbef842c8433b6da82abb6
Ayrton Porto <22572595+Ikusa0@users.noreply.github.com>
2024-01-27 17:26:37 -0300

    feat: add landing's Presentation desktop view

--- src/presentation/pages/landing/components/presentation/presentation.tsx
+++ src/presentation/pages/landing/components/presentation/presentation.tsx
@@ -13,7 +13,7 @@ const Presentation: React.FC = () => {
         <h1>{`More than just shorter links`}</h1>
         <p>
           {`Build your brand's recognition and get detailed insights on how your links are
-            performing`}
+            performing.`}
         </p>
         <GetStartedButton />
       </div>
