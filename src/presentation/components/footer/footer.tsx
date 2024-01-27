df7fe63158585ed2c2e757b460b5b8fc9b44ae7a
Ayrton Porto <22572595+Ikusa0@users.noreply.github.com>
2024-01-27 18:40:09 -0300

    feat: add Footer desktop view

--- src/presentation/components/footer/footer.tsx
+++ src/presentation/components/footer/footer.tsx
@@ -11,58 +11,62 @@ import Styles from './footer-styles.module.scss'
 const Footer: React.FC = () => {
   return (
     <footer className={Styles.footer}>
-      <LogoSVG className={Styles.logo} />
-      <section className={Styles.footerItem}>
-        <h5>{`Features`}</h5>
-        <ul>
-          <li>
-            <a href="#">{`Link Shortening`}</a>
-          </li>
-          <li>
-            <a href="#">{`Branded Links`}</a>
-          </li>
-          <li>
-            <a href="#">{`Analytics`}</a>
-          </li>
-        </ul>
-      </section>
-      <section className={Styles.footerItem}>
-        <h5>{`Resources`}</h5>
-        <ul>
-          <li>
-            <a href="#">{`Blog`}</a>
-          </li>
-          <li>
-            <a href="#">{`Developers`}</a>
-          </li>
-          <li>
-            <a href="#">{`Support`}</a>
-          </li>
-        </ul>
-      </section>
-      <section className={Styles.footerItem}>
-        <h5>{`Company`}</h5>
-        <ul>
-          <li>
-            <a href="#">{`About`}</a>
-          </li>
-          <li>
-            <a href="#">{`Our Team`}</a>
-          </li>
-          <li>
-            <a href="#">{`Careers`}</a>
-          </li>
-          <li>
-            <a href="#">{`Contact`}</a>
-          </li>
-        </ul>
-      </section>
-      <section className={`${Styles.social} ${Styles.footerItem}`}>
-        <FacebookICON className={Styles.icon} />
-        <TwitterICON className={Styles.icon} />
-        <PinterestICON className={Styles.icon} />
-        <InstagramICON className={Styles.icon} />
-      </section>
+      <div className={Styles.logoWrapper}>
+        <LogoSVG className={Styles.logo} />
+      </div>
+      <div className={Styles.itemsWrapper}>
+        <section className={Styles.footerItem}>
+          <h5>{`Features`}</h5>
+          <ul>
+            <li>
+              <a href="#">{`Link Shortening`}</a>
+            </li>
+            <li>
+              <a href="#">{`Branded Links`}</a>
+            </li>
+            <li>
+              <a href="#">{`Analytics`}</a>
+            </li>
+          </ul>
+        </section>
+        <section className={Styles.footerItem}>
+          <h5>{`Resources`}</h5>
+          <ul>
+            <li>
+              <a href="#">{`Blog`}</a>
+            </li>
+            <li>
+              <a href="#">{`Developers`}</a>
+            </li>
+            <li>
+              <a href="#">{`Support`}</a>
+            </li>
+          </ul>
+        </section>
+        <section className={Styles.footerItem}>
+          <h5>{`Company`}</h5>
+          <ul>
+            <li>
+              <a href="#">{`About`}</a>
+            </li>
+            <li>
+              <a href="#">{`Our Team`}</a>
+            </li>
+            <li>
+              <a href="#">{`Careers`}</a>
+            </li>
+            <li>
+              <a href="#">{`Contact`}</a>
+            </li>
+          </ul>
+        </section>
+        <section className={`${Styles.social} ${Styles.footerItem}`}>
+          <FacebookICON className={Styles.icon} />
+          <TwitterICON className={Styles.icon} />
+          <PinterestICON className={Styles.icon} />
+          <InstagramICON className={Styles.icon} />
+        </section>
+      </div>
     </footer>
   )
 }
